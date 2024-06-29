b878fdb17e5def43d4f609cd50899299dca02db4
Gabytto <gabyttox@gmail.com>
2024-06-29 19:17:32 -0300

    renderizado de bd en tabla donaciones

---
+++ assets/js/dashboard/gestion-donaciones/gestion-donaciones.js
@@ -0,0 +1,57 @@
+const contenedorPadre = document.querySelector(".contenedor-registros");
+const url = "http://localhost:3000/donaciones";
+
+async function solicitarDonaciones(url) {
+  try {
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+      console.log("Error de coneción a la base de datos", respuesta.status);
+      return;
+    }
+    const dato = await respuesta.json();
+
+    return dato;
+  } catch (error) {
+    console.error("Error al solicitar donaciones", error);
+  }
+}
+
+async function renderizarDonaciones(url) {
+  try {
+    const donacionesLista = await solicitarDonaciones(url);
+
+    if (donacionesLista) {
+      iterarDonacionesLista(donacionesLista);
+      return donacionesLista;
+    }
+  } catch (error) {
+    console.log("Error al renderizar donaciones", error);
+  }
+}
+
+function iterarDonacionesLista(donacionesLista) {
+  donacionesLista.map((donacion) => {
+    const donacionContenedor = document.createElement("tr");
+
+    donacionContenedor.setAttribute("data-id", `${donacion.ID}`);
+    const contenidoDonacion = `  
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>`;
+
+    donacionContenedor.innerHTML = contenidoDonacion;
+    contenedorPadre.appendChild(donacionContenedor);
+
+    const campos = donacionContenedor.querySelectorAll("td");
+    campos[0].textContent = donacion.nombre;
+    campos[1].textContent = donacion.email;
+    campos[2].textContent = donacion.fechaDonacion;
+    campos[3].textContent = donacion.montoDonacion;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let donaciones = await renderizarDonaciones(url);
+});
