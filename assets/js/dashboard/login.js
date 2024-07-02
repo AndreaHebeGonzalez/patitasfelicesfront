06d5516fa62da8a59521c72afa36f2d9f7049737
Fernando Viveros <fernandoviveros95@gmail.com>
2024-07-02 11:33:18 -0300

    Login funcionando

--- assets/js/dashboard/login.js
+++ assets/js/dashboard/login.js
@@ -1,23 +1,22 @@
 function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const user = { email: email, password: password };
+    const user = JSON.stringify({ email: email, password: password });
+    const data = new FormData(document.getElementById('login-form'));
+    console.log(data);
     console.log(user);
     fetch('http://localhost:3000/sesion/login', {
         method: 'POST',
-        body: {
-            "email": "fernando@gmail.com",
-            "password": "Fernando123"
-        }
+        body: data
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            console.log(data.token);
             if (data.error) {
                 alert(data.error, "Email o contraseña incorrectos");
             }
             else {
-                document.cookie = "token=" + data;
+                document.cookie = "token3=" + data.token;
             }
         }
         )
